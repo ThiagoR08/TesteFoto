9cde0dc5b3f5d288158b9b0d825a308f73001f0f
Thiago Martins <thiagor08martins@gmail.com>
2020-10-08 10:15:54 -0300

    Teste Git Noturno 2020-2

---
+++ app/src/main/java/com/example/testefoto_01/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.testefoto_01;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnTiraFoto;
+    ImageView imgSuaImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        retiraFoto();
+    }
+    private void retiraFoto(){
+        btnTiraFoto =  findViewById(R.id.btnTiraFoto);
+        imgSuaImage = findViewById(R.id.suaImage);
+
+        btnTiraFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abreCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(abreCamera, 0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        if (data!=null){
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+            imgSuaImage.setImageBitmap(bitmap);
+        }
+    }
+}
